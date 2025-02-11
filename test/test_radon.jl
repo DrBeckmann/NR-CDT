212d83ba0ca76876a1864553270eeb38a9e81a32
Robert Beinert <robert.beinert@tu-berlin.de>
2025-02-11 12:59:05 +0100

    fix: correct the Radon transform of the square

--- test/test_radon.jl
+++ test/test_radon.jl
@@ -1,12 +1,12 @@
 @testset "square phantom" begin
     I = ones(10, 10)
     S = RadonTransform.radon(I, 101, 5, 0.0)
-    @test true == all(isapprox.(S[1:14, 1], 0.0, atol=1e-6))
-    @test true == all(isapprox.(S[15:51, 1], sqrt(2), atol=1e-6))
+    @test true == all(isapprox.(S[1:15, 1], 0.0, atol=1e-6))
+    @test true == all(isapprox.(S[16:51, 1], sqrt(2), atol=1e-6))
     @test true == all(isapprox.(S[1:51, 2], [2t / 50 for t=0:50], atol=1e-6))
-    @test true == all(isapprox.(S[1:14, 3], 0.0, atol=1e-6))
-    @test true == all(isapprox.(S[15:51, 3], sqrt(2), atol=1e-6))
+    @test true == all(isapprox.(S[1:15, 3], 0.0, atol=1e-6))
+    @test true == all(isapprox.(S[16:51, 3], sqrt(2), atol=1e-6))
     @test true == all(isapprox.(S[1:51, 4], [2t / 50 for t=0:50], atol=1e-6))
-    @test true == all(isapprox.(S[1:14, 5], 0.0, atol=1e-6))
-    @test true == all(isapprox.(S[15:51, 5], sqrt(2), atol=1e-6))
+    @test true == all(isapprox.(S[1:15, 5], 0.0, atol=1e-6))
+    @test true == all(isapprox.(S[16:51, 5], sqrt(2), atol=1e-6))
 end
