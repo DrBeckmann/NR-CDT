9dcf727dc9c89cbf66bcbfdba0396d807acfc96b
Robert Beinert <robert.beinert@tu-berlin.de>
2025-02-12 11:34:37 +0100

    perf: remove all packages up to the backbone

--- src/NormalizedRadonCDT.jl
+++ src/NormalizedRadonCDT.jl
@@ -2,15 +2,15 @@ module NormalizedRadonCDT
 
 export TestImages
 export RadonTransform
-export radon_cdt
+#export radon_cdt
 
-using Base.Threads
+#using Base.Threads
 
 include("TestImages.jl")
 include("RadonTransform.jl")
-include("radon_cdt.jl")
-include("Temp.jl")
-include("Data.jl")
-include("Classify.jl")
+#include("radon_cdt.jl")
+#include("Temp.jl")
+#include("Data.jl")
+#include("Classify.jl")
 
 end
