0dcde26f97d54fb3419cf4d7e57148f099f9ac93
JJEWBresch <bresch@math.tu-berlin.de>
2025-02-23 14:03:22 +0100

    feat: num. experiments in Pluto notebook

--- pluto/Tab_4_mnist.jl
+++ pluto/Tab_4_mnist.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.4
+# v0.19.47
 
 using Markdown
 using InteractiveUtils
@@ -72,6 +72,45 @@ NormalizedRadonCDT.classify_flatten_svm(rClass, MLLabel)
 # ╔═╡ d1208fce-2ef8-4243-99bd-ba6a8f3deda1
 NormalizedRadonCDT.classify_flatten_svm(mClass, MLLabel)
 
+# ╔═╡ ec24a204-5a8a-4752-8839-6ff7b35756b1
+# ╠═╡ disabled = true
+#=╠═╡
+gMLClass, gMLLabel = DataTransformations.samp_mnist(trainset, [1, 7], 5000);
+  ╠═╡ =#
+
+# ╔═╡ 44476053-4531-4bc9-9740-44ac54817c44
+# ╠═╡ disabled = true
+#=╠═╡
+gTMLClass = A.(gMLClass);
+  ╠═╡ =#
+
+# ╔═╡ 9dde6e4f-ca20-4843-90fc-19edddd83f75
+# ╠═╡ disabled = true
+#=╠═╡
+for class_size in [10,20,50,250,500,1000,5000]
+	@info "class size:" class_size
+	
+	sTMLClass = gTMLClass[1:class_size];
+	append!(sTMLClass, gTMLClass[5001:5000+class_size]);
+	sMLLabel = gMLLabel[1:class_size];
+	append!(sMLLabel, gMLLabel[5001:5000+class_size]);
+	
+	NormalizedRadonCDT.classify_flatten_svm(sTMLClass, sMLLabel)
+	
+	for angle in [2,4,8,16]
+		R = RadonTransform(floor(Int,sqrt(2)*256),angle,0.0);
+		RCDT = RadonCDT(floor(Int,sqrt(2)*256), R);
+		NRCDT = NormRadonCDT(RCDT);
+		mNRCDT = MaxNormRadonCDT(RCDT);
+		rqMLClass = RCDT.(sTMLClass);
+		mqMLClass = mNRCDT.(sTMLClass);
+		@info "number of equispaced angles:" angle
+		NormalizedRadonCDT.classify_flatten_svm(rqMLClass, sMLLabel)
+		NormalizedRadonCDT.classify_flatten_svm(mqMLClass, sMLLabel)	
+	end
+end
+  ╠═╡ =#
+
 # ╔═╡ Cell order:
 # ╠═8cbe0300-edff-11ef-2fad-d3b8cca171a9
 # ╠═81783bfb-d7a2-4c18-a4f8-b634f3bbc59b
@@ -90,3 +129,6 @@ NormalizedRadonCDT.classify_flatten_svm(mClass, MLLabel)
 # ╠═9bfdf99b-6b93-477c-9954-56409f53774a
 # ╠═051f04d7-dd1d-4a8e-bd68-32199e4216c0
 # ╠═d1208fce-2ef8-4243-99bd-ba6a8f3deda1
+# ╠═ec24a204-5a8a-4752-8839-6ff7b35756b1
+# ╠═44476053-4531-4bc9-9740-44ac54817c44
+# ╠═9dde6e4f-ca20-4843-90fc-19edddd83f75
