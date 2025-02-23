37c8d584393239a15406bb0e44aaebff6a66676f
JJEWBresch <bresch@math.tu-berlin.de>
2025-02-23 14:03:19 +0100

    feat: num. experiments in Pluto notebook

--- pluto/Tab_3_academic.jl
+++ pluto/Tab_3_academic.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.4
+# v0.19.47
 
 using Markdown
 using InteractiveUtils
@@ -40,15 +40,15 @@ J₃ = DataTransformations.extend_image(I₃, (256, 256))
 J = [J₁, J₂]; Label = [1, 2];
 
 # ╔═╡ 14864b75-d2e6-476a-bf63-5ffffa95a61d
-Class, Labels = DataTransformations.generate_academic_classes(J, class_size=30);
+Class, Labels = DataTransformations.generate_academic_classes(J, class_size=10);
 
 # ╔═╡ 773832af-9099-4dcf-bd1b-c82baaa83424
 A = DataTransformations.RandomAffineTransformation(
-	scale_x = (0.75, 1.25), 
-	scale_y = (0.75, 1.25),
+	#scale_x = (0.75, 1.25), 
+	#scale_y = (0.75, 1.25),
 	rotate=(-90.0, 90.0), 
-	shear_x=(-2.0, 2.0),
-	shear_y=(-2.0, 2.0),
+	#shear_x=(-2.0, 2.0),
+	#shear_y=(-2.0, 2.0),
 	shift_x=(-20, 20),
 	shift_y=(-20, 20)
 	)
@@ -57,7 +57,7 @@ A = DataTransformations.RandomAffineTransformation(
 TClass = A.(Class)
 
 # ╔═╡ 8fb1f5c3-386e-4117-9b87-dedb75c1ae1d
-R = RadonTransform(floor(Int,sqrt(2)*256),8,0.0)
+R = RadonTransform(floor(Int,sqrt(2)*256),365,0.0)
 
 # ╔═╡ bbbcd04c-8b4f-4c44-958d-9e4089ada051
 RCDT = RadonCDT(floor(Int,sqrt(2)*256), R)
@@ -80,6 +80,12 @@ qTemp = mNRCDT.(J);
 # ╔═╡ e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
 NormalizedRadonCDT.mNRCDT_quantiles(qTemp, Label, qClass, Labels)
 
+# ╔═╡ 5ed28cfd-baa6-4bd0-808b-bbf8051461ac
+tTClass = [transpose(Array{Float64}(TClass[i])) for i in 1:length(TClass)];
+
+# ╔═╡ 5643d1b8-3565-4125-bfcd-545941513f0b
+NormalizedRadonCDT.classify_flatten_svm(tTClass, Labels)
+
 # ╔═╡ 2a6defcb-81de-44fa-936b-d7871a188d1a
 NormalizedRadonCDT.classify_flatten_svm(TClass, Labels)
 
@@ -88,6 +94,45 @@ NormalizedRadonCDT.classify_flatten_svm(rClass, Labels)
 
 # ╔═╡ ad89e85d-92a8-445a-8e6f-14ed1a570d06
 NormalizedRadonCDT.classify_flatten_svm(qClass, Labels)
+
+# ╔═╡ 457006d5-ae0d-4ee9-b58d-0c0c87d4b128
+# ╠═╡ disabled = true
+#=╠═╡
+gClass, gLabels = DataTransformations.generate_academic_classes(J, class_size=270);
+  ╠═╡ =#
+
+# ╔═╡ c974aee4-ba38-4103-ab3d-b9d1a29066e9
+# ╠═╡ disabled = true
+#=╠═╡
+gTClass = A.(gClass);
+  ╠═╡ =#
+
+# ╔═╡ 59720153-0f97-4144-befa-0cff41aee075
+# ╠═╡ disabled = true
+#=╠═╡
+for class_size in [10,30,90,270]
+	@info "class size:" class_size
+	
+	sTClass = gTClass[1:class_size];
+	append!(sTClass, gTClass[271:270+class_size]);
+	sLabels = gLabels[1:class_size];
+	append!(sLabels, gLabels[271:270+class_size]);
+	
+	NormalizedRadonCDT.classify_flatten_svm(sTClass, sLabels)
+	
+	for angle in [2,4,8,16]
+		R = RadonTransform(floor(Int,sqrt(2)*256),angle,0.0);
+		RCDT = RadonCDT(floor(Int,sqrt(2)*256), R);
+		NRCDT = NormRadonCDT(RCDT);
+		mNRCDT = MaxNormRadonCDT(RCDT);
+		rqClass = RCDT.(sTClass);
+		mqClass = mNRCDT.(sTClass);
+		@info "number of equispaced angles:" angle
+		NormalizedRadonCDT.classify_flatten_svm(rqClass, sLabels)
+		NormalizedRadonCDT.classify_flatten_svm(mqClass, sLabels)	
+	end
+end
+  ╠═╡ =#
 
 # ╔═╡ Cell order:
 # ╠═8cbe0300-edff-11ef-2fad-d3b8cca171a9
@@ -109,6 +154,11 @@ NormalizedRadonCDT.classify_flatten_svm(qClass, Labels)
 # ╠═553a0f34-84b4-4997-b00d-c90fdb1ae833
 # ╠═6f89e4df-7af6-4fdd-bf50-029b07ca82c2
 # ╠═e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
+# ╠═5ed28cfd-baa6-4bd0-808b-bbf8051461ac
+# ╠═5643d1b8-3565-4125-bfcd-545941513f0b
 # ╠═2a6defcb-81de-44fa-936b-d7871a188d1a
 # ╠═67396c87-3bf4-4189-9a6b-9f861600a425
 # ╠═ad89e85d-92a8-445a-8e6f-14ed1a570d06
+# ╠═457006d5-ae0d-4ee9-b58d-0c0c87d4b128
+# ╠═c974aee4-ba38-4103-ab3d-b9d1a29066e9
+# ╠═59720153-0f97-4144-befa-0cff41aee075
