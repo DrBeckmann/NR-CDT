dff7773b3e1b9162dbc737a75e3075cd026e15c5
JJEWBresch <bresch@math.tu-berlin.de>
2025-02-24 09:14:49 +0100

    feat: small changes

--- pluto/Tab_3_academic.jl
+++ pluto/Tab_3_academic.jl
@@ -36,28 +36,29 @@ J₃ = DataTransformations.extend_image(I₃, (256, 256))
 
 # ╔═╡ c1959bcd-b5ce-40ae-9c41-142fca3027b6
 # J = [J₁, J₂, J₃]; Label = [1, 2, 3];
-#J = [J₂, J₃]; Label = [1, 2];
+# J = [J₂, J₃]; Label = [1, 2];
 J = [J₁, J₂]; Label = [1, 2];
 
 # ╔═╡ 14864b75-d2e6-476a-bf63-5ffffa95a61d
-Class, Labels = DataTransformations.generate_academic_classes(J, class_size=10);
+Class, Labels = DataTransformations.generate_academic_classes(J, class_size=90);
 
 # ╔═╡ 773832af-9099-4dcf-bd1b-c82baaa83424
 A = DataTransformations.RandomAffineTransformation(
-	#scale_x = (0.75, 1.25), 
-	#scale_y = (0.75, 1.25),
+	scale_x = (0.75, 1.25), 
+	scale_y = (0.75, 1.25),
 	rotate=(-90.0, 90.0), 
-	#shear_x=(-2.0, 2.0),
-	#shear_y=(-2.0, 2.0),
+	shear_x=(-2.0, 2.0),
+	shear_y=(-2.0, 2.0),
 	shift_x=(-20, 20),
 	shift_y=(-20, 20)
 	)
 
 # ╔═╡ fb3629dc-1860-4a96-a75e-2b4402f847fe
+# TClass = NormalizedRadonCDT.norm_data(A.(Class))
 TClass = A.(Class)
 
 # ╔═╡ 8fb1f5c3-386e-4117-9b87-dedb75c1ae1d
-R = RadonTransform(floor(Int,sqrt(2)*256),365,0.0)
+R = RadonTransform(floor(Int,sqrt(2)*256),4,0.0)
 
 # ╔═╡ bbbcd04c-8b4f-4c44-958d-9e4089ada051
 RCDT = RadonCDT(floor(Int,sqrt(2)*256), R)
@@ -79,12 +80,6 @@ qTemp = mNRCDT.(J);
 
 # ╔═╡ e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
 NormalizedRadonCDT.mNRCDT_quantiles(qTemp, Label, qClass, Labels)
-
-# ╔═╡ 5ed28cfd-baa6-4bd0-808b-bbf8051461ac
-tTClass = [transpose(Array{Float64}(TClass[i])) for i in 1:length(TClass)];
-
-# ╔═╡ 5643d1b8-3565-4125-bfcd-545941513f0b
-NormalizedRadonCDT.classify_flatten_svm(tTClass, Labels)
 
 # ╔═╡ 2a6defcb-81de-44fa-936b-d7871a188d1a
 NormalizedRadonCDT.classify_flatten_svm(TClass, Labels)
@@ -154,8 +149,6 @@ end
 # ╠═553a0f34-84b4-4997-b00d-c90fdb1ae833
 # ╠═6f89e4df-7af6-4fdd-bf50-029b07ca82c2
 # ╠═e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
-# ╠═5ed28cfd-baa6-4bd0-808b-bbf8051461ac
-# ╠═5643d1b8-3565-4125-bfcd-545941513f0b
 # ╠═2a6defcb-81de-44fa-936b-d7871a188d1a
 # ╠═67396c87-3bf4-4189-9a6b-9f861600a425
 # ╠═ad89e85d-92a8-445a-8e6f-14ed1a570d06
