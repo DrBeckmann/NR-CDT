ed1196bf85499cddfba375347c4482e10c50dd75
JJEWBresch <bresch@math.tu-berlin.de>
2025-02-26 01:06:10 +0100

    refactor: main module

--- src/NormalizedRadonCDT.jl
+++ src/NormalizedRadonCDT.jl
@@ -7,16 +7,12 @@ export TestImages
 export RadonCDT, RadonTransform
 export NormRadonCDT, MaxNormRadonCDT, MeanNormRadonCDT
 export RandomAffineTransformation
-#export radon_cdt
 
-#using Base.Threads
 
 include("TestImages.jl")
 include("RadonTransform.jl")
 include("RadonCDT.jl")
 include("DataTransformations.jl")
-#include("Temp.jl")
-#include("Data.jl")
 include("Classify.jl")
 
 end
