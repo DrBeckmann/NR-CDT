ca9a9080a1dd255124507989a53d20865f090bdf
Robert Beinert <robert.beinert@tu-berlin.de>
2025-03-11 13:36:54 +0100

    doc: add details to the SSVM experiment Tab3

--- pluto/ssvm2025/Tab3.jl
+++ pluto/ssvm2025/Tab3.jl
@@ -17,6 +17,25 @@ begin
 	using Plots
 end
 
+# ╔═╡ 92cfdfdf-cfac-40bd-9bf5-bc44b3942e98
+md"""
+# SSVM 2025 -- Table 3
+This pluto notebook reproduces the numerical experiment
+for Table 3 from
+
+- Matthias Beckmann, Robert Beinert, Jonas Bresch, 
+  'Max-Normalized Radon Cumulative Distribution
+  Transform for Limited Data Classification',
+  SSVM 2025.
+"""
+
+# ╔═╡ 5d298c1f-c203-4f31-b904-0af64d3bd7da
+md"""
+## Templates
+Generate the two templates
+using the submodule `TestImages`.
+"""
+
 # ╔═╡ c9a1f57f-1874-40e4-b47f-d66f7dd4a064
 I₁ = render(OrbAndCross(Circle(),Star(1)));
 
@@ -29,19 +48,20 @@ I₂ = render(OrbAndCross(Square(),Star(4)));
 # ╔═╡ 29d43338-99a6-42ce-9cf6-eee91d3905b8
 J₂ = extend_image(I₂, (256, 256))
 
-# ╔═╡ e6c9fb45-2ec7-4925-bc2c-efbed91caa46
-I₃ = render(Shield(Triangle()));
-
-# ╔═╡ 25220f99-8cbd-4387-b4fd-bb4a0e6fad96
-J₃ = extend_image(I₃, (256, 256))
-
 # ╔═╡ c1959bcd-b5ce-40ae-9c41-142fca3027b6
-# J = [J₁, J₂, J₃]; Label = [1, 2, 3];
-# J = [J₂, J₃]; Label = [1, 2];
 J = [J₁, J₂]; Label = [1, 2];
 
+# ╔═╡ 19430770-d572-4e16-9191-70ae876be076
+md"""
+## Dataset
+Generate the dataset 
+by duplicating the templates
+and by applying random affine transformations
+using the submodule `DataTransformations`.
+"""
+
 # ╔═╡ 14864b75-d2e6-476a-bf63-5ffffa95a61d
-Class, Labels = generate_academic_classes(J, class_size=90);
+Class, Labels = generate_academic_classes(J, class_size=270);
 
 # ╔═╡ 773832af-9099-4dcf-bd1b-c82baaa83424
 A = DataTransformations.RandomAffineTransformation(
@@ -54,105 +74,60 @@ A = DataTransformations.RandomAffineTransformation(
 	shift_y=(-20, 20)
 	)
 
-# ╔═╡ fb3629dc-1860-4a96-a75e-2b4402f847fe
-# TClass = NormalizedRadonCDT.norm_data(A.(Class))
+# ╔═╡ c974aee4-ba38-4103-ab3d-b9d1a29066e9
 TClass = A.(Class)
 
-# ╔═╡ 8fb1f5c3-386e-4117-9b87-dedb75c1ae1d
-R = RadonTransform(floor(Int,sqrt(2)*256),4,0.0)
-
-# ╔═╡ bbbcd04c-8b4f-4c44-958d-9e4089ada051
-RCDT = RadonCDT(floor(Int,sqrt(2)*256), R)
-
-# ╔═╡ 81fd55d8-24df-4047-b235-20468b2c111c
-NRCDT = NormRadonCDT(RCDT)
-
-# ╔═╡ 81e32395-78d9-4a5f-b6f0-ba2d6f01c8ee
-mNRCDT = MaxNormRadonCDT(RCDT)
-
-# ╔═╡ f956f9c4-ab3d-4c55-9eae-d0e082a9b188
-rClass = RCDT.(TClass);
-
-# ╔═╡ 553a0f34-84b4-4997-b00d-c90fdb1ae833
-qClass = mNRCDT.(TClass);
-
-# ╔═╡ 6f89e4df-7af6-4fdd-bf50-029b07ca82c2
-qTemp = mNRCDT.(J);
-
-# ╔═╡ e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
-plot_quantiles(qTemp, Label, qClass, Labels)
-
-# ╔═╡ 2a6defcb-81de-44fa-936b-d7871a188d1a
-accuracy_cross_svm(TClass, Labels)
-
-# ╔═╡ 67396c87-3bf4-4189-9a6b-9f861600a425
-accuracy_cross_svm(rClass, Labels)
-
-# ╔═╡ ad89e85d-92a8-445a-8e6f-14ed1a570d06
-accuracy_cross_svm(qClass, Labels)
-
-# ╔═╡ 457006d5-ae0d-4ee9-b58d-0c0c87d4b128
-# ╠═╡ disabled = true
-#=╠═╡
-gClass, gLabels = generate_academic_classes(J, class_size=270);
-  ╠═╡ =#
-
-# ╔═╡ c974aee4-ba38-4103-ab3d-b9d1a29066e9
-# ╠═╡ disabled = true
-#=╠═╡
-gTClass = A.(gClass);
-  ╠═╡ =#
+# ╔═╡ 8e54cefd-8133-41f9-ae1e-69968bd1c9e5
+md"""
+## Linear SVM Classification -- Table 3
+Train a linear support vector machine (SVM)
+to classify the generated dataset.
+The first SVM employes the Euclidean representations,
+the second SVM the plain RCDT projections,
+and the third SVM the max-normalized RCDTs.
+We perform a 10-fold cross-validation,
+where one tenth is used for training
+and nine tenth for testing.
+The experiment is repeated 
+for different class sizes
+and different numbers of used angles.
+"""
 
 # ╔═╡ 59720153-0f97-4144-befa-0cff41aee075
-# ╠═╡ disabled = true
-#=╠═╡
 for class_size in [10,30,90,270]
 	@info "class size:" class_size
 	
-	sTClass = gTClass[1:class_size];
-	append!(sTClass, gTClass[271:270+class_size]);
-	sLabels = gLabels[1:class_size];
-	append!(sLabels, gLabels[271:270+class_size]);
+	sTClass = TClass[1:class_size];
+	append!(sTClass, TClass[271:270+class_size]);
+	sLabels = Labels[1:class_size];
+	append!(sLabels, Labels[271:270+class_size]);
 	
-	classify_flatten_svm(sTClass, sLabels)
+	accuracy_cross_svm(sTClass, sLabels)
 	
-	for angle in [2,4,8,16]
-		R = RadonTransform(floor(Int,sqrt(2)*256),angle,0.0);
-		RCDT = RadonCDT(floor(Int,sqrt(2)*256), R);
-		NRCDT = NormRadonCDT(RCDT);
-		mNRCDT = MaxNormRadonCDT(RCDT);
-		rqClass = RCDT.(sTClass);
-		mqClass = mNRCDT.(sTClass);
+	for angle in [2,4,8]
+		R = RadonTransform(floor(Int,sqrt(2)*256),angle,0.0)
+		RCDT = RadonCDT(floor(Int,sqrt(2)*256), R)
+		mNRCDT = MaxNormRadonCDT(RCDT)
+		rqClass = RCDT.(sTClass)
+		mqClass = mNRCDT.(sTClass)
 		@info "number of equispaced angles:" angle
 		accuracy_cross_svm(rqClass, sLabels)
 		accuracy_cross_svm(mqClass, sLabels)	
 	end
 end
-  ╠═╡ =#
 
 # ╔═╡ Cell order:
+# ╟─92cfdfdf-cfac-40bd-9bf5-bc44b3942e98
 # ╠═8cbe0300-edff-11ef-2fad-d3b8cca171a9
+# ╟─5d298c1f-c203-4f31-b904-0af64d3bd7da
 # ╠═c9a1f57f-1874-40e4-b47f-d66f7dd4a064
 # ╠═237a6d1c-6d30-40a0-8eb6-aa0ae913b6d2
 # ╠═8ab0ffae-2f4c-4b8b-b201-7f86d9ef25ac
 # ╠═29d43338-99a6-42ce-9cf6-eee91d3905b8
-# ╠═e6c9fb45-2ec7-4925-bc2c-efbed91caa46
-# ╠═25220f99-8cbd-4387-b4fd-bb4a0e6fad96
 # ╠═c1959bcd-b5ce-40ae-9c41-142fca3027b6
+# ╟─19430770-d572-4e16-9191-70ae876be076
 # ╠═14864b75-d2e6-476a-bf63-5ffffa95a61d
 # ╠═773832af-9099-4dcf-bd1b-c82baaa83424
-# ╠═fb3629dc-1860-4a96-a75e-2b4402f847fe
-# ╠═8fb1f5c3-386e-4117-9b87-dedb75c1ae1d
-# ╠═bbbcd04c-8b4f-4c44-958d-9e4089ada051
-# ╠═81fd55d8-24df-4047-b235-20468b2c111c
-# ╠═81e32395-78d9-4a5f-b6f0-ba2d6f01c8ee
-# ╠═f956f9c4-ab3d-4c55-9eae-d0e082a9b188
-# ╠═553a0f34-84b4-4997-b00d-c90fdb1ae833
-# ╠═6f89e4df-7af6-4fdd-bf50-029b07ca82c2
-# ╠═e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
-# ╠═2a6defcb-81de-44fa-936b-d7871a188d1a
-# ╠═67396c87-3bf4-4189-9a6b-9f861600a425
-# ╠═ad89e85d-92a8-445a-8e6f-14ed1a570d06
-# ╠═457006d5-ae0d-4ee9-b58d-0c0c87d4b128
 # ╠═c974aee4-ba38-4103-ab3d-b9d1a29066e9
+# ╟─8e54cefd-8133-41f9-ae1e-69968bd1c9e5
 # ╠═59720153-0f97-4144-befa-0cff41aee075
