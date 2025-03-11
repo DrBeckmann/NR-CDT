ac5e7693bc928e4716d7b57b6863d0681e18b231
Robert Beinert <robert.beinert@tu-berlin.de>
2025-03-11 12:46:03 +0100

    doc: add details to the SSVM experiments in Tab 2

--- pluto/ssvm2025/Tab2-academic.jl
+++ pluto/ssvm2025/Tab2-academic.jl
@@ -17,6 +17,25 @@ begin
 	using Plots
 end
 
+# ╔═╡ bdf92971-170c-42ab-a5d9-9fe5ce3fdc6c
+md"""
+# SSVM 2025 -- Table 2 (left), Figure 4
+This pluto notebook reproduces the numerical experiment
+for Table 2 (left) and Figure 4 from
+
+- Matthias Beckmann, Robert Beinert, Jonas Bresch, 
+  'Max-Normalized Radon Cumulative Distribution
+  Transform for Limited Data Classification',
+  SSVM 2025.
+"""
+
+# ╔═╡ 5b58bf37-7019-4b8d-9dd4-36c04040a393
+md"""
+## Templates
+Generate the three templates
+using the submodule `TestImages`.
+"""
+
 # ╔═╡ c9a1f57f-1874-40e4-b47f-d66f7dd4a064
 I₁ = render(OrbAndCross(Circle(),Star(1)));
 
@@ -39,6 +58,15 @@ J₃ = extend_image(I₃, (256, 256))
 J = [J₁, J₂, J₃]; Label = [1, 2, 3];
 # J = [J₂, J₃]; Label = [1, 2];
 
+# ╔═╡ 0a3ccf51-e303-4f8d-b627-0676df4f561d
+md"""
+## Dataset
+Generate the dataset 
+by duplicating the templates
+and by applying random affine transformations
+using the submodule `DataTransformations`.
+"""
+
 # ╔═╡ 14864b75-d2e6-476a-bf63-5ffffa95a61d
 Class, Labels = generate_academic_classes(J, class_size=10);
 
@@ -55,14 +83,19 @@ A = DataTransformations.RandomAffineTransformation(
 # ╔═╡ fb3629dc-1860-4a96-a75e-2b4402f847fe
 TClass = A.(Class)
 
+# ╔═╡ 16aaed33-371e-428f-8ba5-fd56d4ee1b76
+md"""
+## Max-Normalized RCDT -- Figure 4
+Setup the max-normalized RCDT,
+and apply it 
+to the dataset and templates.
+"""
+
 # ╔═╡ 8fb1f5c3-386e-4117-9b87-dedb75c1ae1d
 R = RadonTransform(floor(Int,sqrt(2)*256),120,0.0)
 
 # ╔═╡ bbbcd04c-8b4f-4c44-958d-9e4089ada051
 RCDT = RadonCDT(256, R)
-
-# ╔═╡ 81fd55d8-24df-4047-b235-20468b2c111c
-NRCDT = NormRadonCDT(RCDT)
 
 # ╔═╡ 81e32395-78d9-4a5f-b6f0-ba2d6f01c8ee
 mNRCDT = MaxNormRadonCDT(RCDT)
@@ -73,33 +106,40 @@ mqClass = mNRCDT.(TClass);
 # ╔═╡ 6f89e4df-7af6-4fdd-bf50-029b07ca82c2
 mqTemp = mNRCDT.(J);
 
+# ╔═╡ b03bd8d5-eeaf-4713-85e3-c392bf59dd32
+md"""
+Plot the computed max-normalized RCDTs.
+"""
+
 # ╔═╡ e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
 plot_quantiles(mqTemp, Label, mqClass, Labels)
 
-# ╔═╡ d6a239d6-d73c-41dc-9ea1-60e6c881f968
-accuracy_nearest_neighbour(mqTemp, Label, mqClass, Labels, "euclidean", ret=1);
-
-# ╔═╡ c806713e-5074-4f2f-ad43-43e1083a6d11
-accuracy_nearest_neighbour(mqTemp, Label, mqClass, Labels, "inf", ret=1);
+# ╔═╡ 536d2775-bb0f-48c9-b51a-e67208803c75
+md"""
+## Nearest Neighbour Classification -- Table 2
+Use the nearest neighbour classification
+with respect to the chosen templates
+to classify the generated dataset.
+The max-normalized RCDT is applied
+with different numbers of used angles.
+"""
 
 # ╔═╡ b0a056d3-d727-4a5d-bcae-264d58f6cae2
-# ╠═╡ disabled = true
-#=╠═╡
 for angle in [2,4,8,16,32,64,128]
-	R = RadonTransform(floor(Int,sqrt(2)*256),angle,0.0);
-	RCDT = RadonCDT(floor(Int,sqrt(2)*256), R);
-	NRCDT = NormRadonCDT(RCDT);
-	mNRCDT = MaxNormRadonCDT(RCDT);
-	mqClass = mNRCDT.(TClass);
-	mqTemp = mNRCDT.(J);
+	R = RadonTransform(floor(Int,sqrt(2)*256),angle,0.0)
+	RCDT = RadonCDT(floor(Int,sqrt(2)*256), R)
+	mNRCDT = MaxNormRadonCDT(RCDT)
+	mqClass = mNRCDT.(TClass)
+	mqTemp = mNRCDT.(J)
 	@info "number of equispaced angles:" angle
-	accuracy_nearest_neighbour(mqTemp, Label, mqClass, Labels, "inf", ret=1);
-	accuracy_nearest_neighbour(mqTemp, Label, mqClass, Labels, "euclidean", ret=1);
+	accuracy_nearest_neighbour(mqTemp, Label, mqClass, Labels, "inf", ret=1)
+	accuracy_nearest_neighbour(mqTemp, Label, mqClass, Labels, "euclidean", ret=1)
 end
-  ╠═╡ =#
 
 # ╔═╡ Cell order:
+# ╟─bdf92971-170c-42ab-a5d9-9fe5ce3fdc6c
 # ╠═8cbe0300-edff-11ef-2fad-d3b8cca171a9
+# ╟─5b58bf37-7019-4b8d-9dd4-36c04040a393
 # ╠═c9a1f57f-1874-40e4-b47f-d66f7dd4a064
 # ╠═237a6d1c-6d30-40a0-8eb6-aa0ae913b6d2
 # ╠═8ab0ffae-2f4c-4b8b-b201-7f86d9ef25ac
@@ -107,16 +147,17 @@ end
 # ╠═e6c9fb45-2ec7-4925-bc2c-efbed91caa46
 # ╠═25220f99-8cbd-4387-b4fd-bb4a0e6fad96
 # ╠═c1959bcd-b5ce-40ae-9c41-142fca3027b6
+# ╟─0a3ccf51-e303-4f8d-b627-0676df4f561d
 # ╠═14864b75-d2e6-476a-bf63-5ffffa95a61d
 # ╠═773832af-9099-4dcf-bd1b-c82baaa83424
 # ╠═fb3629dc-1860-4a96-a75e-2b4402f847fe
+# ╟─16aaed33-371e-428f-8ba5-fd56d4ee1b76
 # ╠═8fb1f5c3-386e-4117-9b87-dedb75c1ae1d
 # ╠═bbbcd04c-8b4f-4c44-958d-9e4089ada051
-# ╠═81fd55d8-24df-4047-b235-20468b2c111c
 # ╠═81e32395-78d9-4a5f-b6f0-ba2d6f01c8ee
 # ╠═553a0f34-84b4-4997-b00d-c90fdb1ae833
 # ╠═6f89e4df-7af6-4fdd-bf50-029b07ca82c2
+# ╟─b03bd8d5-eeaf-4713-85e3-c392bf59dd32
 # ╠═e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
-# ╠═d6a239d6-d73c-41dc-9ea1-60e6c881f968
-# ╠═c806713e-5074-4f2f-ad43-43e1083a6d11
+# ╟─536d2775-bb0f-48c9-b51a-e67208803c75
 # ╠═b0a056d3-d727-4a5d-bcae-264d58f6cae2
--- pluto/ssvm2025/Tab2-mnist.jl
+++ pluto/ssvm2025/Tab2-mnist.jl
@@ -18,14 +18,72 @@ begin
 	using MLDatasets
 end
 
+# ╔═╡ da709924-bbbc-4233-80f8-03b4c2a22376
+md"""
+# SSVM 2025 -- Table 2 (right)
+This pluto notebook reproduces the numerical experiment
+for Table 2 (right) from
+
+- Matthias Beckmann, Robert Beinert, Jonas Bresch, 
+  'Max-Normalized Radon Cumulative Distribution
+  Transform for Limited Data Classification',
+  SSVM 2025.
+"""
+
+# ╔═╡ 92f547a1-1f91-4e31-a746-bb81969a728f
+md"""
+## Dataset
+Load MNIST 
+using `MLDatasets`.
+Further information about MNIST can be found in
+
+- L. Deng, 
+  '[The MNIST database of handwritten digit images 
+  for machine learning research]
+  (https://doi.org/10.1109/MSP.2012.2211477)',
+  *IEEE Signal Processing Magazine* **29**(6),
+  141--142 (2012). 
+
+!!! warning "Download MNIST"
+	In order to load MNIST
+	using `MLDatasets`,
+	MNIST has to be downloaded first!
+	For this,
+	one can activate the enivironment of the project
+	and load the dataset one time explicitly.
+	Starting a Julia REPL 
+	in the main directory of the project,
+	this can be done by
+	```
+	import Pkg
+	Pkg.activate
+	using MLDatasets
+	MNIST(:train)
+	```
+	Julia then asks to download the corresponding dataset.
+"""
+
 # ╔═╡ 81783bfb-d7a2-4c18-a4f8-b634f3bbc59b
 trainset = MNIST(:train)
 
-# ╔═╡ 98131234-5ab0-4954-bd68-0646241ed22a
-typeof(trainset)
+# ╔═╡ 9876c55a-c5eb-4902-bf59-92f242a393a6
+md"""
+Generate LinMNIST
+by selecting a subset of samples
+and applying random affine transformations
+using the submodule `DataTransformations`.
+Further information about LinMNIST can be found in
+
+- M. Beckmann, N. Heilenkötter,
+  '[Equivariant neural networks 
+  for indirect measurements]
+  (https://doi.org/10.1137/23M1582862)',
+  *SIAM Journal on Mathematics of Data Science* **6**(3),
+  579--601 (2024).
+"""
 
 # ╔═╡ 81170d86-6140-41ce-a1e4-24e70c0530ff
-MLClass, MLLabel = generate_ml_classes(trainset, [1, 5, 7], 50);
+MLClass, MLLabel = generate_ml_classes(trainset, [1, 5, 7], 10);
 
 # ╔═╡ bf8448db-7cb1-42ba-9f1e-03b775b31cb8
 MLClass
@@ -43,59 +101,38 @@ A = DataTransformations.RandomAffineTransformation(
 # ╔═╡ fb3629dc-1860-4a96-a75e-2b4402f847fe
 TMLClass = A.(MLClass)
 
-# ╔═╡ 8fb1f5c3-386e-4117-9b87-dedb75c1ae1d
-R = RadonTransform(256,8,0.0)
-
-# ╔═╡ bbbcd04c-8b4f-4c44-958d-9e4089ada051
-RCDT = RadonCDT(256, R)
-
-# ╔═╡ 81fd55d8-24df-4047-b235-20468b2c111c
-NRCDT = NormRadonCDT(RCDT)
-
-# ╔═╡ 81e32395-78d9-4a5f-b6f0-ba2d6f01c8ee
-mNRCDT = MaxNormRadonCDT(RCDT)
-
-# ╔═╡ 553a0f34-84b4-4997-b00d-c90fdb1ae833
-qClass = mNRCDT.(TMLClass);
-
-# ╔═╡ e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
-plot_quantiles([qClass[1], qClass[11], qClass[21]], [1, 5, 7], qClass, MLLabel)
-
-# ╔═╡ 2a28b3ba-60c5-4a5c-9aee-e3c342748f75
-accuracy_nearest_cross_neighbour(qClass, MLLabel, "inf")
-
-# ╔═╡ 47ef7d01-cef9-4be3-b494-b12a459b5b89
-accuracy_nearest_cross_neighbour(qClass, MLLabel, "euclidean")
+# ╔═╡ 8b0121f4-9506-4af8-89ee-2fd2932c24a2
+md"""
+## Nearest Neighbour Classification -- Table 2
+Use the nearest neighbour classification
+with respect to five randomly chosen representatives
+per class
+to classify the generated dataset.
+The max-normalized RCDT is applied
+with different numbers of used angles.
+Each experiment is repeated ten times.
+"""
 
 # ╔═╡ 348b704c-c1b5-456b-ad6b-d19a5057e84b
-# ╠═╡ disabled = true
-#=╠═╡
 for angle in [2,4,8,16,32,64,128]
-	R = RadonTransform(floor(Int,sqrt(2)*256),angle,0.0);
-	RCDT = RadonCDT(floor(Int,sqrt(2)*256), R);
-	NRCDT = NormRadonCDT(RCDT);
-	mNRCDT = MaxNormRadonCDT(RCDT);
-	qClass = mNRCDT.(TMLClass);
+	R = RadonTransform(floor(Int,sqrt(2)*256),angle,0.0)
+	RCDT = RadonCDT(floor(Int,sqrt(2)*256), R)
+	mNRCDT = MaxNormRadonCDT(RCDT)
+	qClass = mNRCDT.(TMLClass)
 	@info "number of equispaced angles:" angle
 	accuracy_nearest_cross_neighbour(qClass, MLLabel, "inf")
 	accuracy_nearest_cross_neighbour(qClass, MLLabel, "euclidean")
 end
-  ╠═╡ =#
 
 # ╔═╡ Cell order:
+# ╟─da709924-bbbc-4233-80f8-03b4c2a22376
 # ╠═8cbe0300-edff-11ef-2fad-d3b8cca171a9
+# ╟─92f547a1-1f91-4e31-a746-bb81969a728f
 # ╠═81783bfb-d7a2-4c18-a4f8-b634f3bbc59b
-# ╠═98131234-5ab0-4954-bd68-0646241ed22a
+# ╟─9876c55a-c5eb-4902-bf59-92f242a393a6
 # ╠═81170d86-6140-41ce-a1e4-24e70c0530ff
 # ╠═bf8448db-7cb1-42ba-9f1e-03b775b31cb8
 # ╠═773832af-9099-4dcf-bd1b-c82baaa83424
 # ╠═fb3629dc-1860-4a96-a75e-2b4402f847fe
-# ╠═8fb1f5c3-386e-4117-9b87-dedb75c1ae1d
-# ╠═bbbcd04c-8b4f-4c44-958d-9e4089ada051
-# ╠═81fd55d8-24df-4047-b235-20468b2c111c
-# ╠═81e32395-78d9-4a5f-b6f0-ba2d6f01c8ee
-# ╠═553a0f34-84b4-4997-b00d-c90fdb1ae833
-# ╠═e1d7ede5-1b44-4186-9b9e-21e6ddd29dda
-# ╠═2a28b3ba-60c5-4a5c-9aee-e3c342748f75
-# ╠═47ef7d01-cef9-4be3-b494-b12a459b5b89
+# ╟─8b0121f4-9506-4af8-89ee-2fd2932c24a2
 # ╠═348b704c-c1b5-456b-ad6b-d19a5057e84b
