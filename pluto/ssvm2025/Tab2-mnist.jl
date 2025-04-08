a288ea81cc5a2eeefe29501f5755d698251ae065
JJEWBresch <bresch@math.tu-berlin.de>
2025-04-08 17:42:25 +0200

    fix: number of samples per class

--- pluto/ssvm2025/Tab2-mnist.jl
+++ pluto/ssvm2025/Tab2-mnist.jl
@@ -83,7 +83,7 @@ Further information about LinMNIST can be found in
 """
 
 # ╔═╡ 81170d86-6140-41ce-a1e4-24e70c0530ff
-MLClass, MLLabel = generate_ml_classes(trainset, [1, 5, 7], 10);
+MLClass, MLLabel = generate_ml_classes(trainset, [1, 5, 7], 50);
 
 # ╔═╡ bf8448db-7cb1-42ba-9f1e-03b775b31cb8
 MLClass
