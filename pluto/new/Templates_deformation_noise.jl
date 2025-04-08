ac8ec41d0982f2f792b255c4b52d2aa4f6d2c13f
JJEWBresch <bresch@math.tu-berlin.de>
2025-04-08 17:48:30 +0200

    add: notebook for the optimal transport plans

---
+++ pluto/new/Templates_deformation_noise.jl
@@ -0,0 +1,204 @@
+### A Pluto.jl notebook ###
+# v0.19.47
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 03f9300e-692e-11ef-145c-85ecce1e4c7f
+begin
+	import Pkg
+	Pkg.activate("..")
+	using Revise
+	using NormalizedRadonCDT
+	using NormalizedRadonCDT.TestImages
+	using NormalizedRadonCDT.DataTransformations
+	using NormalizedRadonCDT.Classify
+	using Images
+	using Plots
+end
+
+# ╔═╡ ea97ef35-3aa2-421c-a919-0c775266dbd5
+using JLD, PythonOT
+
+# ╔═╡ e2ba028e-6346-4a9b-8bd3-9223a9c537d9
+I₁ = render(OrbAndCross(Square(),Star(4)));
+
+# ╔═╡ 43c617ec-3abd-42ef-80ef-62cc80d4dee7
+J₁ = extend_image(I₁, (256, 256))
+
+# ╔═╡ f7f1f2de-af31-4bd3-9e23-f6bc55c4e8d8
+savefig(plot(Gray.(J₁), axis=false, grid=false), "org_img.pdf")
+
+# ╔═╡ cd3f1705-772f-41d0-a37e-ba5cd345712d
+A = DataTransformations.RandomAffineTransformation(
+	scale_x = (0.75, 1.25), 
+	scale_y = (0.75, 1.25),
+	rotate=(-90.0, 90.0), 
+	shear_x=(-10.0, 10.0),
+	shear_y=(-10.0, 10.0),
+	shift_x=(-20, 20),
+	shift_y=(-20, 20)
+	)
+
+# ╔═╡ 144852d9-16d8-46f7-81a2-300e1d8e3c30
+E = DataTransformations.ElasticNoise(
+	amplitude_x=(2.5, 7.5), 
+	amplitude_y=(2.5, 7.5),
+	frequency_x=(0.5, 2.0),
+	frequency_y=(0.5, 2.0))
+
+# ╔═╡ 9056755a-b5c4-4a3a-8525-02738200a117
+S = DataTransformations.SaltNoise((3,7), (3/128, 3/128))
+
+# ╔═╡ 9fe89baa-4882-4af3-b3f1-06c7a5311fe3
+N = DataTransformations.MikadoNoise((2,10), (0.125,0.5), (0.1,1))
+
+# ╔═╡ 10196a6b-ae6b-4999-a9f8-a8761e8cd8b7
+savefig(plot(J₁), "orig_img.pdf")
+
+# ╔═╡ 4e012794-7f50-4e38-884f-65f4458c2f94
+T = S(E(J₁))
+
+# ╔═╡ 8932eb05-932d-4a06-ba5b-4a1e6dffb435
+temp_img = Array{Float64}(J₁); temp_dis = Array{Float64}(T);
+
+# ╔═╡ 4c3fa402-ac0f-41f7-a432-c285a56dbbd9
+savefig(plot(Gray.(T), axis=false, grid=false), "dis_img.pdf")
+
+# ╔═╡ 27fea2a5-2389-4cc5-b1a8-ce2f9ea17f97
+nk_temp_img = findall(x->x>1e-6, temp_img); mk_temp_dis = findall(x->x>1e-6, temp_dis); 
+
+# ╔═╡ 01afc537-717b-4af1-a7db-e9859d6a7acb
+temp_img_s = temp_img[nk_temp_img]; temp_dis_s = temp_dis[mk_temp_dis];
+
+# ╔═╡ c26e759e-30c0-4e80-a220-86343a4b1162
+temp_img_v = vec(temp_img_s); temp_dis_v = vec(temp_dis_s);
+
+# ╔═╡ 4d93083c-64a8-4746-8236-79f8cc897525
+temp_img_flat = temp_img_v/sum(temp_img_v); temp_dis_flat = temp_dis_v/sum(temp_dis_v);
+
+# ╔═╡ 6b228522-2498-425c-8d70-7de7473d6570
+n = size(temp_img_flat)[1]; m = size(temp_dis_flat)[1];
+
+# ╔═╡ 071901d2-fedf-4d3a-a270-7abe23f4d733
+C = zeros(n,m); np = 2;
+
+# ╔═╡ a699c2aa-efad-43a3-9bfb-f62867089dab
+for i in 1:n
+	# for j in 1:n
+		for k in 1:m
+			# for l in 1:m
+				a = nk_temp_img[i][1]
+				b = nk_temp_img[i][2]
+				c = mk_temp_dis[k][1]
+				d = mk_temp_dis[k][2]
+				C[i, k] = abs(a - c)^np + abs(b - d)^np
+				# C[i, k] = max(abs(a - c), abs(b - d))
+			# end
+		end
+	# end
+end
+
+# ╔═╡ 7d197833-aee6-4012-8eaf-e597891bb476
+P = emd(temp_img_flat, temp_dis_flat, C);
+
+# ╔═╡ 78176a36-2eeb-476e-ab23-f849ae2ad3b3
+heatmap(P)
+
+# ╔═╡ 80690413-7709-4556-920f-eaa7006ebc7f
+heatmap(C/maximum(C))
+
+# ╔═╡ 3ef25658-b833-4950-9575-241f54b532f5
+cP = emd2(temp_img_flat, temp_dis_flat, C)
+
+# ╔═╡ abe4accd-4819-4be2-9969-2bfdba580be9
+l_x = []; l_y = []; l_u = []; l_v = []; l_s = []; l_w = [];
+
+# ╔═╡ 42583760-0733-4638-bc77-9bb021585a89
+for i in 1:n
+	# for j in 1:m
+		# if temp_img[i,j] > 1e-6
+			xy_pos = findall(x->x>1e-6, P[i,:])
+			s_xy_pos = size(xy_pos)[1]
+			for l in 1:s_xy_pos
+				append!(l_s, s_xy_pos)
+				
+				append!(l_x, nk_temp_img[i][1])
+				append!(l_y, nk_temp_img[i][2])
+
+				append!(l_u, mk_temp_dis[xy_pos[l]][1])
+				append!(l_v, mk_temp_dis[xy_pos[l]][2])
+
+				append!(l_w, P[i,xy_pos[l]])
+			end
+		# end
+	# end
+end
+
+# ╔═╡ f24e1609-bc89-447b-ad74-ca6767a953e3
+q = quiver(l_y,256*ones(size(l_v)[1])-l_x, quiver=(l_v-l_y,-l_u+l_x), width=1.2*l_w/maximum(l_w), 
+	# marker=(:circle, 0.1, :green), 
+	aspect_ratio=:equal,
+	# la=1
+	la=0.1,
+	color=:gray,
+	dpi=250,
+	grid=false,
+	axis=false
+	)
+
+# ╔═╡ cfabffe2-3b9a-4858-8ed3-d79bcc08936b
+savefig(q, "movement_particals.pdf")
+
+# ╔═╡ 7fe07976-f180-4c47-a3b6-cf636901ebe9
+s = scatter(l_y, 256*ones(size(l_x)[1])-l_x, color=:green, ms=1, ma=0.2, label = "original image");
+
+# ╔═╡ bad075bd-35f7-43ab-87b5-6017df2d5b8b
+plot(scatter(s, l_v, 256*ones(size(l_u)[1])-l_u, color=:red, ms=1, ma=0.2, label = "disturbed image"));
+
+# ╔═╡ 648d5829-352f-4a3f-ba9a-6803d4065f27
+qq = quiver!(l_y, 256*ones(size(l_x)[1])-l_x, quiver=(l_v-l_y, -l_u+l_x), width=1.2*l_w/maximum(l_w), 
+	# marker=(:circle, 0.1, :green), 
+	aspect_ratio=:equal,
+	# la=1
+	color=:gray,
+	la=0.2,
+	dpi=250
+	)
+
+# ╔═╡ 1418d1c6-b027-428a-a508-ad556c2c551d
+savefig(qq, "movement_img_particals.pdf")
+
+# ╔═╡ Cell order:
+# ╠═03f9300e-692e-11ef-145c-85ecce1e4c7f
+# ╠═ea97ef35-3aa2-421c-a919-0c775266dbd5
+# ╠═e2ba028e-6346-4a9b-8bd3-9223a9c537d9
+# ╠═43c617ec-3abd-42ef-80ef-62cc80d4dee7
+# ╠═f7f1f2de-af31-4bd3-9e23-f6bc55c4e8d8
+# ╠═cd3f1705-772f-41d0-a37e-ba5cd345712d
+# ╠═144852d9-16d8-46f7-81a2-300e1d8e3c30
+# ╠═9056755a-b5c4-4a3a-8525-02738200a117
+# ╠═9fe89baa-4882-4af3-b3f1-06c7a5311fe3
+# ╠═10196a6b-ae6b-4999-a9f8-a8761e8cd8b7
+# ╠═4e012794-7f50-4e38-884f-65f4458c2f94
+# ╠═8932eb05-932d-4a06-ba5b-4a1e6dffb435
+# ╠═4c3fa402-ac0f-41f7-a432-c285a56dbbd9
+# ╠═27fea2a5-2389-4cc5-b1a8-ce2f9ea17f97
+# ╠═01afc537-717b-4af1-a7db-e9859d6a7acb
+# ╠═c26e759e-30c0-4e80-a220-86343a4b1162
+# ╠═4d93083c-64a8-4746-8236-79f8cc897525
+# ╠═6b228522-2498-425c-8d70-7de7473d6570
+# ╠═071901d2-fedf-4d3a-a270-7abe23f4d733
+# ╠═a699c2aa-efad-43a3-9bfb-f62867089dab
+# ╠═7d197833-aee6-4012-8eaf-e597891bb476
+# ╠═78176a36-2eeb-476e-ab23-f849ae2ad3b3
+# ╠═80690413-7709-4556-920f-eaa7006ebc7f
+# ╠═3ef25658-b833-4950-9575-241f54b532f5
+# ╠═abe4accd-4819-4be2-9969-2bfdba580be9
+# ╠═42583760-0733-4638-bc77-9bb021585a89
+# ╠═f24e1609-bc89-447b-ad74-ca6767a953e3
+# ╠═cfabffe2-3b9a-4858-8ed3-d79bcc08936b
+# ╠═7fe07976-f180-4c47-a3b6-cf636901ebe9
+# ╠═bad075bd-35f7-43ab-87b5-6017df2d5b8b
+# ╠═648d5829-352f-4a3f-ba9a-6803d4065f27
+# ╠═1418d1c6-b027-428a-a508-ad556c2c551d
