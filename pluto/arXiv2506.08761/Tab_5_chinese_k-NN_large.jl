5583bc165a5a250e9e8bfe32e5c3ef4bc53144b3
JJEWBresch <bresch@math.tu-berlin.de>
2025-08-20 21:08:28 +0200

    fix: affine transformation settign, use local CASIA dataset

--- pluto/arXiv2506.08761/Tab_5_chinese_k-NN_large.jl
+++ pluto/arXiv2506.08761/Tab_5_chinese_k-NN_large.jl
@@ -15,7 +15,7 @@ begin
 	using NormalizedRadonCDT.Classify
 	using Images
 	using Plots
-	using Glob
+	using JLD2
 	using Random
 	Random.seed!(42)
 end
@@ -34,64 +34,20 @@ for Table 5 (lower right block) from
 # ╔═╡ 6b753037-4286-407d-823b-7ff08f7aa505
 md"""
 ## Dataset
-Load the Chinese hand-written character dataset `handwritten-chinese-character-hanzi-datasets`.
-Further information can be found in
+Load a selection of the Chinese hand-written character dataset `CASIA Chinese Handwriting Databases`.
+For Further information we refer to '[Handwritten chinese character hanzi datasets] (https://nlpr.ia.ac.cn/databases/handwriting/Home.html)'. 
 
-- P. Bliem, '[Handwritten chinese character hanzi datasets] (https://www.kaggle.com/datasets/pascalbliem/handwritten-chinese-character-hanzi-datasets)', (2022), Accessed: March 26, 2025.  
-
-!!! warning "Download the Chinese handwritten character dataset"
-	In order to load the dataset
-	using `kaggle`,
-	it has to be loaded first!
-	For this,
-	one can activate the enivironment of the project
-	and load the dataset one time explicitly.
-	Starting a Julia REPL 
-	in the main directory of the project,
-	this can be done by
-	```
-	import kaggle
-	# This will use your credentials from ~/.kaggle/kaggle.json
-	from kaggle.api.kaggle_api_extended import KaggleApi
-	api = KaggleApi()
-	api.authenticate()
-	api.dataset_download_files('pascalbliem/handwritten-chinese-character-hanzi-datasets', path='./data', unzip=True)
-	```
-	Julia then asks to download the corresponding dataset.
+- C.-L. Liu, F. Yin, D.-H. Wang, Q.-F. Wang, CASIA online and offline Chinese handwriting databases, *Proc. 11th International Conference on Document Analysis and Recognition (ICDAR)*, Beijing, China, 2011, pp.37-41.
 """
 
-# ╔═╡ 1911d0f8-219a-40ba-a2f6-ba353e14eef4
-md"
-- navigate the dataset to the currect path, i.e. the folder in which the notbook runs.
-- the following steps collect from each of the first 1000 classes the first handwritten symbol 
-"
-
-# ╔═╡ 2450a95a-b230-4b73-b369-3aa36d3172b5
-root_folder = "chinese_character";
-
-# ╔═╡ 782377c4-21bf-4f9f-a8ba-4b3bf849c696
-subfolders = filter(name -> isdir(joinpath(root_folder, name)), readdir(root_folder));
-
-# ╔═╡ 639e3aed-dde6-491c-a4a3-8be172766755
-chinese_character = [];
-
-# ╔═╡ dbe82aad-857e-45db-b56f-35e0acffde0c
-for sub in subfolders[1:1000]
-    subpath = joinpath(root_folder, sub)
-    # Get all .png files in the subfolder
-     image_files = Glob.glob("*.png", subpath)
-    
-    if !isempty(image_files)
-        # Load the first image from the subfolder
-        first_image = image_files[1]
-        # println("Loading first image from subfolder ", sub, ": ", first_image)
-        img = load(first_image)
-        # Display the image (or process as needed)
-        # display(img)
-		push!(chinese_character, 1 .- img)
-    else
-        println("No image found in subfolder ", sub)
-    end
+# ╔═╡ c1c19c32-1219-427e-a156-a1b408c3ed9e
+begin
+	dict = JLD2.load("../../data/CASIA_selection/chinese_character.jld2");
+	chinese_character = dict["chinese_character"];
+	for k in 1:length(chinese_character)
+		chinese_character[k] = 1 .- chinese_character[k]
+	end
+	ext_chinese_character = extend_image.(chinese_character, 128)
 end
 
 # ╔═╡ ff5ba6ba-3657-4642-8db5-c04f9a0454c8
@@ -101,19 +57,16 @@ Generate the 1000 templates
 using the submodule `TestImages`.
 """
 
-# ╔═╡ 8f5dfb6e-9c0d-4187-9fd1-1c0acccc4ac1
-ext_chinese_character = extend_image.(chinese_character, 128)
-
 # ╔═╡ 28cbf21b-37a0-4307-bad7-7ff6d1efd511
-Class, Labels = generate_academic_classes(ext_chinese_character, 1:1000, class_size=50);
+Class, Labels = generate_academic_classes(ext_chinese_character[1:1000], 1:1000, class_size=50);
 
 # ╔═╡ fc1f2b19-fe55-4421-a904-398a2448597b
 A = DataTransformations.RandomAffineTransformation(
-	scale_x = (0.75, 1.25), 
-	scale_y = (0.75, 1.25),
-	rotate=(-45.0, 45.0), 
-	shear_x=(-5.0, 5.0),
-	shear_y=(-5.0, 5.0),
+	scale_x = (0.5, 1.0), 
+	scale_y = (0.5, 1.0),
+	rotate=(-180.0, 180.0), 
+	shear_x=(-25.0, 25.0),
+	shear_y=(-25.0, 25.0),
 	shift_x=(-20, 20),
 	shift_y=(-20, 20))
 
@@ -195,13 +148,8 @@ end
 # ╟─efcdfcff-cef6-48c9-992e-8a2eb9348424
 # ╠═19587268-0828-11f0-01fa-e979f61f03a3
 # ╟─6b753037-4286-407d-823b-7ff08f7aa505
-# ╟─1911d0f8-219a-40ba-a2f6-ba353e14eef4
-# ╠═2450a95a-b230-4b73-b369-3aa36d3172b5
-# ╠═782377c4-21bf-4f9f-a8ba-4b3bf849c696
-# ╠═639e3aed-dde6-491c-a4a3-8be172766755
-# ╠═dbe82aad-857e-45db-b56f-35e0acffde0c
+# ╠═c1c19c32-1219-427e-a156-a1b408c3ed9e
 # ╟─ff5ba6ba-3657-4642-8db5-c04f9a0454c8
-# ╠═8f5dfb6e-9c0d-4187-9fd1-1c0acccc4ac1
 # ╠═28cbf21b-37a0-4307-bad7-7ff6d1efd511
 # ╠═fc1f2b19-fe55-4421-a904-398a2448597b
 # ╠═7e5f32b7-2f27-4877-a6fe-c6b41750aa1b
--- pluto/arXiv2506.08761/Tab_5_chinese_k-NN_small.jl
+++ pluto/arXiv2506.08761/Tab_5_chinese_k-NN_small.jl
@@ -15,7 +15,7 @@ begin
 	using NormalizedRadonCDT.Classify
 	using Images
 	using Plots
-	using Glob
+	using JLD2
 	using Random
 	Random.seed!(42)
 end
@@ -34,64 +34,20 @@ for Table 5 (upper right block) from
 # ╔═╡ 6b753037-4286-407d-823b-7ff08f7aa505
 md"""
 ## Dataset
-Load the Chinese hand-written character dataset `handwritten-chinese-character-hanzi-datasets`.
-Further information can be found in
+Load a selection of the Chinese hand-written character dataset `CASIA Chinese Handwriting Databases`.
+For Further information we refer to '[Handwritten chinese character hanzi datasets] (https://nlpr.ia.ac.cn/databases/handwriting/Home.html)'. 
 
-- P. Bliem, '[Handwritten chinese character hanzi datasets] (https://www.kaggle.com/datasets/pascalbliem/handwritten-chinese-character-hanzi-datasets)', (2022), Accessed: March 26, 2025.  
-
-!!! warning "Download the Chinese handwritten character dataset"
-	In order to load the dataset
-	using `kaggle`,
-	it has to be loaded first!
-	For this,
-	one can activate the enivironment of the project
-	and load the dataset one time explicitly.
-	Starting a Julia REPL 
-	in the main directory of the project,
-	this can be done by
-	```
-	import kaggle
-	# This will use your credentials from ~/.kaggle/kaggle.json
-	from kaggle.api.kaggle_api_extended import KaggleApi
-	api = KaggleApi()
-	api.authenticate()
-	api.dataset_download_files('pascalbliem/handwritten-chinese-character-hanzi-datasets', path='./data', unzip=True)
-	```
-	Julia then asks to download the corresponding dataset.
+- C.-L. Liu, F. Yin, D.-H. Wang, Q.-F. Wang, CASIA online and offline Chinese handwriting databases, *Proc. 11th International Conference on Document Analysis and Recognition (ICDAR)*, Beijing, China, 2011, pp.37-41.
 """
 
-# ╔═╡ db221814-64e6-46c0-a288-b435876a4ed2
-md"
-- navigate the dataset to the currect path, i.e. the folder in which the notbook runs.
-- the following steps collect from each of the first 100 classes the first handwritten symbol 
-"
-
-# ╔═╡ 2450a95a-b230-4b73-b369-3aa36d3172b5
-root_folder = "chinese_character";
-
-# ╔═╡ 782377c4-21bf-4f9f-a8ba-4b3bf849c696
-subfolders = filter(name -> isdir(joinpath(root_folder, name)), readdir(root_folder));
-
-# ╔═╡ 639e3aed-dde6-491c-a4a3-8be172766755
-chinese_character = [];
-
-# ╔═╡ dbe82aad-857e-45db-b56f-35e0acffde0c
-for sub in subfolders[1:100]
-    subpath = joinpath(root_folder, sub)
-    # Get all .png files in the subfolder
-     image_files = Glob.glob("*.png", subpath)
-    
-    if !isempty(image_files)
-        # Load the first image from the subfolder
-        first_image = image_files[1]
-        # println("Loading first image from subfolder ", sub, ": ", first_image)
-        img = load(first_image)
-        # Display the image (or process as needed)
-        # display(img)
-		push!(chinese_character, 1 .- img)
-    else
-        println("No image found in subfolder ", sub)
-    end
+# ╔═╡ 0b7d3e09-9166-45c8-9497-40ed74c45995
+begin
+	dict = JLD2.load("../../data/CASIA_selection/chinese_character.jld2");
+	chinese_character = dict["chinese_character"];
+	for k in 1:length(chinese_character)
+		chinese_character[k] = 1 .- chinese_character[k]
+	end
+	ext_chinese_character = extend_image.(chinese_character, 128)
 end
 
 # ╔═╡ e4ebd3f1-1e9f-4bd0-8beb-eb8e2ff1c2db
@@ -101,19 +57,16 @@ Generate the 100 templates
 using the submodule `TestImages`.
 """
 
-# ╔═╡ 8f5dfb6e-9c0d-4187-9fd1-1c0acccc4ac1
-ext_chinese_character = extend_image.(chinese_character, 128)
-
 # ╔═╡ 28cbf21b-37a0-4307-bad7-7ff6d1efd511
-Class, Labels = generate_academic_classes(ext_chinese_character, 1:100, class_size=50);
+Class, Labels = generate_academic_classes(ext_chinese_character[1:100], 1:100, class_size=50);
 
 # ╔═╡ fc1f2b19-fe55-4421-a904-398a2448597b
 A = DataTransformations.RandomAffineTransformation(
-	scale_x = (0.75, 1.25), 
-	scale_y = (0.75, 1.25),
-	rotate=(-45.0, 45.0), 
-	shear_x=(-5.0, 5.0),
-	shear_y=(-5.0, 5.0),
+	scale_x = (0.5, 1.0), 
+	scale_y = (0.5, 1.0),
+	rotate=(-180.0, 180.0), 
+	shear_x=(-25.0, 25.0),
+	shear_y=(-25.0, 25.0),
 	shift_x=(-20, 20),
 	shift_y=(-20, 20))
 
@@ -195,13 +148,8 @@ end
 # ╟─8de4c852-8bdf-4a34-83c3-c5ddd3826ed6
 # ╠═19587268-0828-11f0-01fa-e979f61f03a3
 # ╟─6b753037-4286-407d-823b-7ff08f7aa505
-# ╟─db221814-64e6-46c0-a288-b435876a4ed2
-# ╠═2450a95a-b230-4b73-b369-3aa36d3172b5
-# ╠═782377c4-21bf-4f9f-a8ba-4b3bf849c696
-# ╠═639e3aed-dde6-491c-a4a3-8be172766755
-# ╠═dbe82aad-857e-45db-b56f-35e0acffde0c
+# ╠═0b7d3e09-9166-45c8-9497-40ed74c45995
 # ╟─e4ebd3f1-1e9f-4bd0-8beb-eb8e2ff1c2db
-# ╠═8f5dfb6e-9c0d-4187-9fd1-1c0acccc4ac1
 # ╠═28cbf21b-37a0-4307-bad7-7ff6d1efd511
 # ╠═fc1f2b19-fe55-4421-a904-398a2448597b
 # ╠═7e5f32b7-2f27-4877-a6fe-c6b41750aa1b
