40e97b88e65df11318dc778bb67e6f6895655b43
Matthias Beckmann <research@mbeckmann.de>
2025-08-20 22:25:29 +0200

    fix: change class size to 10 as in paper

--- pluto/arXiv2506.08761/Fig_7_academic_salt_noise.jl
+++ pluto/arXiv2506.08761/Fig_7_academic_salt_noise.jl
@@ -68,7 +68,7 @@ using the submodule `DataTransformations`.
 """
 
 # ╔═╡ 14864b75-d2e6-476a-bf63-5ffffa95a61d
-Class, Labels = generate_academic_classes(J, Label, class_size=50);
+Class, Labels = generate_academic_classes(J, Label, class_size=10);
 
 # ╔═╡ 773832af-9099-4dcf-bd1b-c82baaa83424
 A = DataTransformations.RandomAffineTransformation(
